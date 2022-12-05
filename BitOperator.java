ef9ce3d4cc7a65e3350792da652aa691fb71ba42
SHIYOU-ASUS\kmrry <shuushuutan@gmail.com>
2022-12-05 18:48:03 +0900

    位元運算

---
+++ BitOperator.java
@@ -0,0 +1,10 @@
+//Bit Operator
+
+public class BitOperator {
+    public static void main(String[]args){
+        //2&3 operator
+        //& = if both of the same = 1, if not = 0
+        //2 = 0010() ,3 = 11 A:10
+        System.out.println(2&3);
+    }
+}
