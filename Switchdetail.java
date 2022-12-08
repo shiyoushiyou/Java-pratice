a558cf1dfc16a0de08330a74578c31ddd148e260
SHIYOU-ASUS\kmrry <shuushuutan@gmail.com>
2022-12-08 23:47:30 +0900

    switch的細節

---
+++ Switchdetail.java
@@ -0,0 +1,13 @@
+public class Switchdetail {
+    public static void main(String[]args){
+        char c = 'a';
+        switch(c){
+            case 'a':
+            System.out.println("ok!");
+            break;
+    }
+}
+// note 1 :when use the "switch" syntax, constant have be the same type.
+//note 2: switch () ← the retrun value have to be the type as follows;(byte,short,char,enum,String)
+//note 3: the switch syntax in conditional statements couldn't use variable it have to be constant.
+}
