b25d75bc5ab3d3839bf813945e1fab22ec7886b8
SHIYOU-ASUS\kmrry <shuushuutan@gmail.com>
2022-12-15 01:07:26 +0900

    while loop

---
+++ whileExercise.java
@@ -0,0 +1,30 @@
+public class whileExercise {
+    public static void main(String[]args){
+        int i = 1;
+        int contant = 3;
+        int Number =101;
+        while(i<Number){
+            if(i%contant ==0){
+            System.out.println(i);
+            }
+        i++;
+
+        }
+    }
+}
+
+class Exercise02{
+    public static void main(String[]args){
+        int i =40;
+        int Number = 200;
+        int DoubleNumber = 2;
+        while(i<=Number){
+            if(i%DoubleNumber==0){
+                System.out.println(i);
+
+            }
+        i++;
+
+        }
+    }
+}
