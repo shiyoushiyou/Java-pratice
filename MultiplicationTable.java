7ae2fddd09f141b21d5d3b220e73631e4e4238e2
SHIYOU-ASUS\kmrry <shuushuutan@gmail.com>
2022-12-15 01:08:11 +0900

    99乘法表

---
+++ MultiplicationTable.java
@@ -0,0 +1,11 @@
+
+public class MultiplicationTable {
+    public static void main(String[]args){
+        int One = 1;
+        for(;One<=9;One++){
+            for (int Number = One ;Number<= 9;Number++){
+            System.out.println(One+" * "+Number+" = "+(One*Number));    
+        }
+    }
+    }
+}
