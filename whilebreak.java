b96f0e396bcde2a65c697ce74e4fdcbb2548339d
SHIYOU-ASUS\kmrry <shuushuutan@gmail.com>
2023-01-05 13:07:26 +0800

    當錢包的錢大於5萬就支付錢包總金額的5%當過路費，小於5萬就支付1千，計算可以經過幾次。

---
+++ whilebreak.java
@@ -0,0 +1,29 @@
+public class whilebreak{
+    public static void main(String[]args){
+        int Money = 100000;
+        int count = 0;
+        while(Money!=0){
+        if (Money>50000){
+            int Payment = Money/20;
+            System.out.println("Please pay the money! "+Payment);
+            Money -= Payment;
+            System.out.println("Available Balance: "+Money);
+            count += 1;        
+        }
+        else if(Money<=50000 && Money>1000)
+        {
+            int LowPayment = Money-1000;
+            System.out.println("Available Balance: "+LowPayment);
+            Money = LowPayment;
+            count += 1;       
+        }
+        else {
+            System.out.println("You don't have available balance,please chrage");
+            break;
+        }
+        System.out.println("You could take the bus "+count+" times in total.");
+        }
+
+
+    }
+}
